5e00ee23d1f513ec78cffe8b46eec917f8001a52
Derek Chen <derex1987@gmail.com>
2021-08-07 18:59:22 -0500

    added copper alehcmy

--- src/main/java/me/derec/transmutation/CopperTransmutator.java
+++ src/main/java/me/derec/transmutation/CopperTransmutator.java
@@ -1,9 +1,12 @@
 package me.derec.transmutation;
 
+import io.github.thebusybiscuit.slimefun4.api.events.PlayerRightClickEvent;
+import io.github.thebusybiscuit.slimefun4.core.handlers.BlockUseHandler;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Objects.Category;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 public class CopperTransmutator extends SlimefunItem
@@ -12,4 +15,22 @@ public class CopperTransmutator extends SlimefunItem
     {
         super(category, item, recipeType, recipe);
     }
+
+    @Override
+    public void preRegister()
+    {
+        //Item handler?
+        BlockUseHandler guyRightClick = this::guyRightClicksThing;
+        addItemHandler(guyRightClick);
+
+    }
+
+    private void guyRightClicksThing(PlayerRightClickEvent event)
+    {
+        if(event.getPlayer().getInventory().contains(Material.IRON_NUGGET))
+        {
+            event.getPlayer().getInventory().addItem(new ItemStack((Material.IRON_INGOT)));
+        }
+    }
+
 }
